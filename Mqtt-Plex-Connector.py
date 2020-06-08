82c8c6cc09db9b7eefe91909f8ffea3551a46e53
Gustavo Olivares <gustavo.olivares@niwa.co.nz>
2020-06-09 11:36:32 +1200

    Routing Masterton

--- Mqtt-Plex-Connector.py
+++ Mqtt-Plex-Connector.py
@@ -11,13 +11,15 @@ host = 'localhost'
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    print('Something is coming')
     data = request.json
     MQTTtopic = data['topic']
     MQTTmessage = json.dumps(data['message'],separators=(',', ':'))
     MQTTtags = ','.join(data['tags'])
     publish.single(MQTTtopic, MQTTmessage, hostname=host)
     publish.single(MQTTtopic + '/tags', MQTTtags, hostname=host)
+    if '_TAG_9274_' in data['tags']:
+        publish.single('masterton2020/latest', MQTTtopic, hostname=host)
+        publish.single('masterton2020/restart', MQTTtopic, hostname=host)
     return 'OK'
 
 if __name__ == '__main__':
