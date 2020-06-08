93c056d7179af141ef3060fc7ca0bc227b9705d1
Gustavo Olivares <1390863+guolivar@users.noreply.github.com>
2020-06-08 22:29:31 +1200

    It now REQUIRES three parts in the payload: topic (string) : The MQTT topic where the messages go message (JSON): The JSON formatted message to publish tags (list): The list of tags to be published in the topic topic/tags

--- Mqtt-Plex-Connector.py
+++ Mqtt-Plex-Connector.py
@@ -7,18 +7,17 @@ import paho.mqtt.publish as publish
 app = Flask(__name__)
 api = Api(app)
 
-host = 'HOST_OF_MQTT'
-topic = 'plex/'
+host = 'localhost'
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    data = json.loads(request.form['payload'])
-    print(data['event'])
-    publish.single(topic + data['Player']['uuid'], data['event'], hostname=host)
-    publish.single(topic + data['Player']['uuid'] + '/title', data['Metadata']['title'],
-                   hostname=host)
-    publish.single(topic + data['Player']['uuid'] + '/type', data['Metadata']['type'],
-                   hostname=host)
+    print('Something is coming')
+    data = request.json
+    MQTTtopic = data['topic']
+    MQTTmessage = json.dumps(data['message'],separators=(',', ':'))
+    MQTTtags = ','.join(data['tags'])
+    publish.single(MQTTtopic, MQTTmessage, hostname=host)
+    publish.single(MQTTtopic + '/tags', MQTTtags, hostname=host)
     return 'OK'
 
 if __name__ == '__main__':
