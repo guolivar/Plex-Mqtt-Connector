4b07006db3f3b0fd57b5ad4992125192994b2a9f
Gustavo Olivares <gustavo.olivares@niwa.co.nz>
2020-06-09 13:57:32 +1200

    Some flexibility and Arrowtow2020

--- Mqtt-Plex-Connector.py
+++ Mqtt-Plex-Connector.py
@@ -12,15 +12,21 @@ host = 'localhost'
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
-    MQTTtopic = data['topic']
+    MQTTtopic = data['devicename']
     MQTTmessage = json.dumps(data['message'],separators=(',', ':'))
     MQTTtags = ','.join(data['tags'])
     publish.single(MQTTtopic, MQTTmessage, hostname=host)
     publish.single(MQTTtopic + '/tags', MQTTtags, hostname=host)
+    # This is for Masterton2020 (_TAG_9274_)
     if '_TAG_9274_' in data['tags']:
         publish.single('masterton2020/latest', MQTTtopic, hostname=host)
         if 'report' in data['tags']:
             publish.single('masterton2020/restart', MQTTtopic, hostname=host)
+    # This is for Arrowtown2020 (_TAG_9180_)
+    if '_TAG_9180_' in data['tags']:
+        publish.single('arrowtown2020/latest', MQTTtopic, hostname=host)
+        if 'report' in data['tags']:
+            publish.single('arrowtown2020/restart', MQTTtopic, hostname=host)
     return 'OK'
 
 if __name__ == '__main__':
